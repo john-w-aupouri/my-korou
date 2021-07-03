f8b8acac8c86a14ee4a2113b6f8cfd255b863831
john_wirihana_aupouri <johnaupouri963@gmail.com>
2021-07-04 10:46:29 +1200

    added site image

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,53 +1,52 @@
-require(`dotenv`).config()
+require(`dotenv`).config();
 
-const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
-const googleAnalyticsTrackingId = process.env.GOOGLE_ANALYTICS_ID
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
+const googleAnalyticsTrackingId = process.env.GOOGLE_ANALYTICS_ID;
 
 module.exports = {
   siteMetadata: {
-    // You can overwrite values here that are used for the SEO component
-    // You can also add new values here to query them like usual
-    // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitle: `Portfolio`,
+    siteTitleAlt: `Johns Portfolio`,
+    siteImage: `/shadowMe.jpg`
   },
   flags: {
-    FAST_DEV: true,
+    FAST_DEV: true
   },
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-cara`,
       // See the theme's README for all available options
-      options: {},
+      options: {}
     },
     googleAnalyticsTrackingId && {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
-      },
+        trackingId: process.env.GOOGLE_ANALYTICS_ID
+      }
     },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `My Korou Showcase`,
-        short_name: `My Korou`,
-        description: `A showcase of what I have done at Korou Digital Ltd`,
+        name: `My Gatsby Portfolio`,
+        short_name: `My Portfolio`,
+        description: `A full-stack web developers portfolio`,
         start_url: `/`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
         display: `standalone`,
         icons: [
           {
-            src: `/logo1.png`,
+            src: `/android-chrome.png`,
             sizes: `192x192`,
-            type: `image/png`,
+            type: `image/png`
           },
           {
-            src: `/logo1.png`,
+            src: `/android-chrome.png`,
             sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
-      },
+            type: `image/png`
+          }
+        ]
+      }
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-gatsby-cloud`,
@@ -60,8 +59,8 @@ module.exports = {
       options: {
         analyzerMode: `static`,
         reportFilename: `_bundle.html`,
-        openAnalyzer: false,
-      },
-    },
-  ].filter(Boolean),
-}
+        openAnalyzer: false
+      }
+    }
+  ].filter(Boolean)
+};
